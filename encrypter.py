cda45b81a73848a54628688d84d5d65bdd8e9809
Marco Lesmes <exelood@gmail.com>
2020-01-07 20:00:17 -0500

    Alternative decode_unzip sound found file

--- encrypter.py
+++ encrypter.py
@@ -8,15 +8,22 @@ class SoundCoder:
         self.size_chunk = 16
 
     def encrypt_sound_found(self, in_file_path):
-        iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
-        aes = AES.new(self.key, AES.MODE_CBC, iv)
-        file_size = os.stat(in_file_path)[6]
         file_path = in_file_path.split('.')[0]
         temporal_file_path = 'temp.sfc'
         compress_file_path = file_path + '.csf'
 
-        with open(in_file_path, 'rb') as in_file:
-            with open(temporal_file_path, 'wb') as out_file:
+        import zipfile
+
+        compress_file = zipfile.ZipFile(temporal_file_path, 'w')
+        compress_file.write(in_file_path, compress_type=zipfile.ZIP_DEFLATED)
+        compress_file.close()
+
+        iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+        aes = AES.new(self.key, AES.MODE_CBC, iv)
+        file_size = os.stat(in_file_path)[6]
+
+        with open(temporal_file_path, 'rb') as in_file:
+            with open(compress_file_path, 'wb') as out_file:
 
                 out_file.write(struct.pack('<Q', file_size))
                 out_file.write(iv)
@@ -31,48 +38,44 @@ class SoundCoder:
                     encoded = aes.encrypt(data)
                     out_file.write(encoded)
 
-        import zipfile
-
-        compress_file = zipfile.ZipFile(compress_file_path, 'w')
-        compress_file.write(temporal_file_path, compress_type=zipfile.ZIP_DEFLATED)
-        compress_file.close()
-
         if os.path.exists(temporal_file_path):
             os.remove(temporal_file_path)
 
         return True
 
     def decrypt_sound_found_in_memory(self, in_file_path):
-        import zipfile
+        with open(in_file_path, 'rb') as in_file:
+            file_size = struct.unpack('<Q', in_file.read(struct.calcsize('<Q')))[0]
+            iv = in_file.read(16)
+            aes = AES.new(self.key, AES.MODE_CBC, iv)
+            out_file = tempfile.NamedTemporaryFile(delete=False)
 
-        zipfile = zipfile.ZipFile(in_file_path)
-        temporal_folder_path = 'C:\\temp'
-        temporal_file_path = 'C:\\temp\\temp.sfc'
+            while True:
+                data = in_file.read(self.size_chunk)
+                n = len(data)
+                if n == 0:
+                    break
+                decode = aes.decrypt(data)
+                n = len(decode)
+                if file_size > n:
+                    out_file.write(decode)
+                else:
+                    out_file.write(decode[:file_size])  # <- remove padding on last block
+                file_size -= n
 
-        zipfile.extractall(temporal_folder_path)
-        zipfile.close()
+            temp_path = out_file.name[:out_file.name.rfind('\\')]
 
-        if os.path.exists(temporal_file_path):
-            with open(temporal_file_path, 'rb') as in_file:
-                file_size = struct.unpack('<Q', in_file.read(struct.calcsize('<Q')))[0]
-                iv = in_file.read(16)
-                aes = AES.new(self.key, AES.MODE_CBC, iv)
-                out_file = tempfile.NamedTemporaryFile(delete=False)
+            import zipfile
+            zipfile = zipfile.ZipFile(out_file)
+            zipfile.extractall(temp_path)
+            zipfile.close()
 
-                while True:
-                    data = in_file.read(self.size_chunk)
-                    n = len(data)
-                    if n == 0:
-                        break
-                    decode = aes.decrypt(data)
-                    n = len(decode)
-                    if file_size > n:
-                        out_file.write(decode)
-                    else:
-                        out_file.write(decode[:file_size])  # <- remove padding on last block
-                    file_size -= n
+            info_zipfile = zipfile.infolist()
 
-            return out_file.name
-        else:
-            print('Error: File ' + temporal_file_path + ' dont was found.')
-            return False
+            if len(info_zipfile) < 2:
+                info_file_path = info_zipfile[0].filename.replace('/', '\\')
+                print(info_file_path)
+                sf_path = temp_path + '\\' + info_file_path
+                print(sf_path)
+
+                return sf_path
